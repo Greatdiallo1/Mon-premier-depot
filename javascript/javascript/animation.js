0c93744d3621573ef26e251da6e63cee72f861bd
greatdiallo1 <38050909+greatdiallo1@users.noreply.github.com>
2018-06-27 20:50:43 +0200

    Add files via upload

---
+++ javascript/javascript/animation.js
@@ -0,0 +1,8 @@
+var hello = document.querySelector("div", "hellow");
+var bonjour = document.querySelector("div", "bonjour");
+function changer() {
+ hello.style.color ="blue";
+ hello.style.marginTop = "100px";
+ hello.style.marginLeft ="20%";
+ bonjour.style.textAlign = "right";
+}
